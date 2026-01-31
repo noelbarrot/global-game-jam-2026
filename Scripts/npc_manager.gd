5eebc8a59ef662a90deed2eaa77c76e7e0ea3b66
Noel Barrot <noelbarrot@gmail.com>
2026-01-31 12:27:03 +0800

    Add opener and reply sending functions

--- Scripts/npc_manager.gd
+++ Scripts/npc_manager.gd
@@ -1,20 +1,26 @@
 extends Node2D
 
 @export var conversations : Array[Conversation]
-
-# Called when the node enters the scene tree for the first time.
+var npcOpening : bool = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("select"):
 		_get_conversation()
-	
 
 func _get_conversation() -> void:
 	var arrayLength : int = conversations.size()
 	var conversationIndex : int = randi_range(0,arrayLength-1)
-	#var conversationIndex : int = 0
 	var loadedConversation : Conversation = conversations[conversationIndex]
-	var conversationOpener: String =loadedConversation.opener.line
-	print(conversationOpener)
+	if npcOpening == true:
+		_send_opener(loadedConversation)
+	else: _send_reply(loadedConversation)
+	
+func _send_opener(chosenConversation) -> Opener:
+	var conversationOpener : Opener = chosenConversation.opener
+	return conversationOpener.Opener
+
+func _send_reply(chosenConversation) -> Reply:
+	var conversationReply : Reply = chosenConversation.reply
+	return conversationReply
 	
